620dcb1a88ac1976a41575fb388c0595f93cbd49
Maheem <88165826+maheem20@users.noreply.github.com>
2023-06-28 13:05:22 +0000

    create callback function

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({
+        success: 1,
+        message: "This is rest api working"
+    });
+});
