ba693ffff06bea61f519ce74e6a4338ab8842fda
maheem_20 <mwistanbuly@gmail.com>
2023-06-29 15:24:04 +0200

    add database connection options

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
 const { createPool } = require('mysql');
 
 const pool = createPool({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test',
+    connectionLimit: 10
 });
