ef5d04b7dc5f5475ce1a2a67edcb6e00e1e7a3cb
maheem_20 <mwistanbuly@gmail.com>
2023-07-03 00:17:38 +0200

    hash password in request body using generated salt

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -4,6 +4,7 @@ module.exports = {
     createUser: (req, res) => {
         const body = req.body;
         const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
         create(body, (err, results) => {
             if (err) {
                 console.log(err);
