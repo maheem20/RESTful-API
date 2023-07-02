7852e8fc09d2c297527123ec168835076462bad0
maheem_20 <mwistanbuly@gmail.com>
2023-07-02 23:55:44 +0200

    call 'create' function from user.service module

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -3,5 +3,7 @@ const { create } = require('./user.service');
 module.exports = {
     createUser: (req, res) => {
         const body = req.body;
+        create(body, (err, results) => {
+        });
     }
 }
