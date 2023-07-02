a24c0c95a3aea831f7be83b3f03bc0a15e79d6a3
maheem_20 <mwistanbuly@gmail.com>
2023-07-02 23:13:38 +0200

    execute an SQL query

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -2,5 +2,8 @@ const pool = require('../config/database');
 
 module.exports = {
     create: (data, callBack) => {
+        pool.query(
+            `insert into registration(firstName, lastName, gender, email, password, number)`
+        )
     }
 }
