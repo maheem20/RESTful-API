5a064faa9a25e52b315ecc6df57efe67f3cc8161
maheem_20 <mwistanbuly@gmail.com>
2023-07-02 23:56:17 +0200

    handling any error that occurred during create operation
    send JSON response with 500 status code and error message

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -4,6 +4,13 @@ module.exports = {
     createUser: (req, res) => {
         const body = req.body;
         create(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: "Database connection error"
+                });
+            }
         });
     }
 }
